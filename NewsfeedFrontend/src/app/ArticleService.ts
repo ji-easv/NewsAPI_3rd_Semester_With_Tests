beb3efc5a7c11a442014117acea7bfa121102662
Julia Ilasova <1julka1il@gmail.com>
2023-09-28 18:20:05 +0200

    added environments

--- NewsfeedFrontend/src/app/ArticleService.ts
+++ NewsfeedFrontend/src/app/ArticleService.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {Article, CreateArticleRequestDto, NewsFeedItem, UpdateArticleRequestDto} from "./Interfaces";
 import {HttpClient} from "@angular/common/http";
 import {firstValueFrom} from "rxjs";
+import {environment} from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -15,20 +16,20 @@ export class ArticleService {
   }
 
   async getArticles() {
-    const call = this.http.get<NewsFeedItem[]>("http://localhost:5000/api/feed");
+    const call = this.http.get<NewsFeedItem[]>(environment.baseUrl + "/api/feed");
     this.articles = await firstValueFrom<NewsFeedItem[]>(call);
     return this.articles;
   }
 
   async getArticle(id: number) {
-    const call = this.http.get<Article>("http://localhost:5000/api/articles/" + id);
+    const call = this.http.get<Article>(environment.baseUrl + "/api/feed" + id);
     const article = await firstValueFrom<Article>(call);
     console.log(article);
     return article;
   }
 
   deleteArticle(articleId: number) {
-    const call = this.http.delete("http://localhost:5000/api/articles/" + articleId);
+    const call = this.http.delete(environment.baseUrl + "/api/feed" + articleId);
     const response = firstValueFrom(call);
     response.then(r => {
         this.getArticles();
@@ -37,7 +38,7 @@ export class ArticleService {
   }
 
   createArticle(article: CreateArticleRequestDto) {
-    const call = this.http.post<Article>("http://localhost:5000/api/articles", article);
+    const call = this.http.post<Article>(environment.baseUrl + "/api/feed", article);
     const response = firstValueFrom<Article>(call);
     response.then(r => {
         this.getArticles();
@@ -46,7 +47,7 @@ export class ArticleService {
   }
 
   updateArticle(articleId: number, article: UpdateArticleRequestDto) {
-    const call = this.http.put<Article>("http://localhost:5000/api/articles/" + articleId, article);
+    const call = this.http.put<Article>(environment.baseUrl + "/api/feed" + articleId, article);
     const response = firstValueFrom<Article>(call);
     response.then(r => {
         this.getArticles();
